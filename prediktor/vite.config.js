4b29d202518ce10f784c8d0158eb6b5cbe1734f2
stevoknievo <38687399+stevoknievo@users.noreply.github.com>
2026-06-02 17:37:25 +0100

    Update vite.config.js

--- prediktor/vite.config.js
+++ prediktor/vite.config.js
@@ -18,9 +18,8 @@ export default defineConfig({
         scope: '/',
         start_url: '/',
         icons: [
-          { src: 'icons/icon-192.png', sizes: '192x192', type: 'image/png' },
-          { src: 'icons/icon-512.png', sizes: '512x512', type: 'image/png' }
-        ]
+  { src: 'favicon.svg', sizes: 'any', type: 'image/svg+xml' }
+]
       }
     })
   ]
