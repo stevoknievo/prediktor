ece50352b458442e241a46545f374de4abc7f8e5
stevoknievo <38687399+stevoknievo@users.noreply.github.com>
2026-06-14 19:41:33 +0100

    Update index.js

--- prediktor/functions/index.js
+++ prediktor/functions/index.js
@@ -283,6 +283,7 @@ exports.syncFixtures = functions.https.onCall(async (data, context) => {
         // Update our existing mXXX document with result data
         apiToOurId[f.id] = ourId
         batch.set(db.collection('fixtures').doc(ourId), {
+          date: f.date,
           completed: f.completed,
           status: f.status,
           hasExtraTime: f.hasExtraTime,
